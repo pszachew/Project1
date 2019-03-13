327b162b99c7821817dc53a5919408e4b51b68c8
Peter <piotrszach@icloud.com>
2019-03-13 21:04:41 +0100

    Added some stuff

--- include/functions.h
+++ include/functions.h
@@ -1,23 +1,10 @@
 #include <iostream>
 #include <set>
+#include "subset.h"
 using namespace std;
-class subset
-{
-private:
-    set<int> container;
-public:
-    void push(int);
-    void push(int, int);
-    void pop(int);
-    void displaySubset();
-    void decrease();
-    void increase();
-    subset operator+(subset);
-    subset operator-(subset);
-};
-void change_value(set<int> *, int);
+void changeValue(set<int> *, int);
 void menu();
-void submenu(subset *, subset *);
+void submenu(Subset *, Subset *);
 
 
 
