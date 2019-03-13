ff51cea9da4c81eda4309f496dd68a4c2c813d96
Piotr Szachewicz <pszachew@lab09.elka.pw.edu.pl>
2019-03-13 22:14:54 +0100

    Deleted unused library

--- include/functions.h
+++ include/functions.h
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <set>
 #include "subset.h"
-using namespace std;
-void changeValue(set<int> *, int);
+void changeValue(std::set<int> *, int);
 void menu();
 void submenu(Subset *, Subset *);
 
